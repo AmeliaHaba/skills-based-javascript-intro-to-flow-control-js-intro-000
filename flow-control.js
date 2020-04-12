84ff92a1a6b1ac8243df69779af7b92b9840f22a
Amelia Haba <ameliahaba347@gmail.com>
2020-04-12 21:43:48 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,7 +25,7 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch(age) {
     case 13:
-      return "You are a teenager" 
+      return "You are a teenager"
     default:
       return "You have an age"
     }
